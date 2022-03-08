8e665c66b3f52ca926bd00f0131b4d2618b21c00
Robson Leal <devleal@hotmail.com>
2022-03-07 22:02:51 -0300

    View bÃasica, retornando um html minimo

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.urls import resolve
 from django.test import TestCase
 from lists.views import home_page
@@ -6,3 +7,12 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpResponse()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
+        
