f42ca55a6edb4aba1c0346477d5655edb95c7a8a
Robson Leal <devleal@hotmail.com>
2022-03-08 15:22:16 -0300

    Refatorando o teste para testar implementaçao e nao constantes

--- lists/tests.py
+++ lists/tests.py
@@ -4,15 +4,7 @@ from django.test import TestCase
 from lists.views import home_page
 
 class HomePageTest(TestCase):
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
-    def test_home_page_returns_correct_html(self):
-        request = HttpResponse()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+    def test_use_home_page(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
         
